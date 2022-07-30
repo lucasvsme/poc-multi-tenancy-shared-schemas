7d8ea1453f941da80e68df58b7849a7ba22162c0
Lucas <lucasvalenteds@gmail.com>
2022-07-30 16:36:08 -0300

    Enabling TenantInterceptor and TenantsContextInitializer

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,15 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
+import com.example.internal.TenantsContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .initializers(new TenantsContextInitializer())
+                .run(args);
     }
 }
